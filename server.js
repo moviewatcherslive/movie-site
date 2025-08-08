be013914950e89dd557f7df3afb8314e9d1f4a09
moviewatcherslive <aakashshukla5546@gmail.com>
2025-08-09 02:56:47 +0530

    Update demo movie site with working files

--- server.js
+++ server.js
@@ -8,15 +8,27 @@ const PORT = process.env.PORT || 3000;
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
-// Static folder
+// Static files
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Home page
+// Demo data (would be replaced by TMDB calls later)
+const demo = {
+  bollywood: [
+    { title: "Bollywood Hit 1", poster: "/images/bollywood1.jpg", trailer: "https://www.youtube.com/embed/ysz5S6PUM-U" },
+    { title: "Bollywood Hit 2", poster: "/images/bollywood2.jpg", trailer: "https://www.youtube.com/embed/ScMzIvxBSi4" }
+  ],
+  hollywood: [
+    { title: "Hollywood Blockbuster 1", poster: "/images/hollywood1.jpg", trailer: "https://www.youtube.com/embed/aqz-KE-bpKQ" },
+    { title: "Hollywood Blockbuster 2", poster: "/images/hollywood2.jpg", trailer: "https://www.youtube.com/embed/2Vv-BfVoq4g" }
+  ],
+  ott: [
+    { title: "OTT Series 1", poster: "/images/ott1.jpg", trailer: "https://www.youtube.com/embed/3fumBcKC6RE" },
+    { title: "OTT Series 2", poster: "/images/ott2.jpg", trailer: "https://www.youtube.com/embed/l9PxOanFjxQ" }
+  ]
+};
+
 app.get('/', (req, res) => {
-  res.render('index'); // views/index.ejs
+  res.render('index', { demo });
 });
 
-// Start server
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+app.listen(PORT, ()=> console.log('Server running on port', PORT));
